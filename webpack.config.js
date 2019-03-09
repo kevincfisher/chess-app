217a5ab12723b22d80b7d43e35ddacc911a09c3f
Kevin Fisher <kfisher@listrak.com>
2019-03-08 23:35:07 -0500

    added babel, webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'chess.js',
+        libraryTarget: 'umd'
+    },
+    module: {
+rules:[
+    {
+        test:/.js$/,
+        exclude: [
+            path.resolve(__dirname, "node_modules")
+        ],
+        loader: 'babel-loader',
+        options: ["preset-es2017"]
+
+    }
+]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
+}
